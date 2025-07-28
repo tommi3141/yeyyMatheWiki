b1f3009969d2534af9c6ae406c232e210cf9c351
tommi3141 <kunz.thomas@posteo.de>
2025-07-28 17:24:01 +0200

    Quartz sync: Jul 28, 2025, 5:24 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,9 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      title: "🌳 Wissensbaum",
+    }),
   ],
   right: [
     Component.Graph(),
@@ -62,7 +64,9 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      title: "🌳 Wissensbaum"
+    }),
   ],
   right: [],
 }
